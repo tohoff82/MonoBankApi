56393a80160e8b08ee0150a1c9822cad651fa04d
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-01 12:18:53 +0300

    Create MonoRequest.cs

---
+++ MonoBankApi/Models/Requests/MonoRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoBankApi.Models.Requests
+{
+    public abstract class MonoRequest
+    {
+        public abstract string Url();
+    }
+}
