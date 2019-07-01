17607aad50ed4c217366babb1c5a8ee4df3beb24
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-01 08:29:45 +0300

    Create CurrencyInfo.cs

---
+++ MonoBankApi/Models/Responses/CurrencyInfo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json
+
+namespace MonoBankApi.Models.Responses
+{
+    public class CurrencyInfo
+    {
+        [JsonProperty("currencyCodeA")]
+        public int CurrencyCodeA { get; set; }
+
+        [JsonProperty("currencyCodeB")]
+        public int CurrencyCodeB { get; set; }
+
+        [JsonProperty("date")]
+        public int Date { get; set; }
+
+        [JsonProperty("rateSell")]
+        public int RateSell { get; set; }
+
+        [JsonProperty("rateBuy")]
+        public int RateBuy { get; set; }
+
+        [JsonProperty("rateCross")]
+        public int RateCross { get; set; }
+    }
+}
