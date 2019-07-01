3c10f628af6020c7ac54169d0157fe6082801e79
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-01 12:19:39 +0300

    Update MonoClient.cs

--- MonoBankApi/MonoClient.cs
+++ MonoBankApi/MonoClient.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MonoBankApi.Models.Requests;
 
 namespace MonoBankApi
 {
-    public class MonoClient
+    public abstract class MonoClient
     {
+        private readonly HttpClient httpClient;
+        private readonly string baseAdress = "https://api.monobank.ua";
+
+        public MonoClient() => httpClient = new HttpClient { BaseAddress = new Uri(baseAdress) };
+        public MonoClient(string token) : this() => httpClient.DefaultRequestHeaders.Add("X-Token", token);
+
+        protected async Task<T> HttpGetAsync<T>(MonoRequest request)
+        {
+            var response = await httpClient.GetAsync(request.Url()).ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            return await UnpackingResponseAsync<T>(response);
+        }
+
+        private async Task<T> UnpackingResponseAsync<T>(HttpResponseMessage responseMessage)
+        {
+            string json = await responseMessage.Content.ReadAsStringAsync();
+            return await Task.Run(() => JsonConvert.DeserializeObject<T>(json));
+        }
+
+        public void Dispose() => httpClient.Dispose();
     }
 }
