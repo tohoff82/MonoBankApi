edf5cf1d8decc2c3ad04eda7f40a11e0445a3624
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-01 12:22:19 +0300

    Summary
    Public methods classes

---
+++ MonoBankApi/IMonoPublic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MonoBankApi.Models.Responses;
+
+namespace MonoBankApi
+{
+    public interface IMonoPublic : IDisposable
+    {
+        Task<List<CurrencyInfoResponse>> ReturnCurrencyInfo();
+    }
+}
---
+++ MonoBankApi/Implements/MonoPublic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MonoBankApi.Models.Requests;
+using MonoBankApi.Models.Responses;
+
+namespace MonoBankApi.Implements
+{
+    public class MonoPublic : MonoClient, IMonoPublic
+    {
+        public MonoPublic() : base() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<CurrencyInfoResponse>> ReturnCurrencyInfo() =>
+            await HttpGetAsync<List<CurrencyInfoResponse>>(new CurrencyInfoRequest());
+    }
+}
