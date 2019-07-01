93b2064c8f07a8225e164245a8e492a47ab1cd3f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-01 13:00:45 +0300

    Summary
    ClientInfo Implements

---
+++ MonoBankApi/IMonoPersonal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using MonoBankApi.Models.Responses;
+
+namespace MonoBankApi
+{
+    public interface IMonoPersonal : IDisposable
+    {
+        Task<ClientInfoResponse> ReturnClientInfo();
+    }
+}
---
+++ MonoBankApi/Implements/MonoPersonal.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using MonoBankApi.Models.Requests;
+using MonoBankApi.Models.Responses;
+
+namespace MonoBankApi.Implements
+{
+    public class MonoPersonal : MonoClient, IMonoPersonal
+    {
+        public MonoPersonal(string token) : base(token) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ClientInfoResponse> ReturnClientInfo() =>
+            await HttpGetAsync<ClientInfoResponse>(new ClientInfoRequest());
+    }
+}
