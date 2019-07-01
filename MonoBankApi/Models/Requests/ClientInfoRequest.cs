b8affcf77abda08abfa566eb1a2cf82b17c6fe3c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-01 12:19:27 +0300

    Create ClientInfoRequest.cs

---
+++ MonoBankApi/Models/Requests/ClientInfoRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace MonoBankApi.Models.Requests
+{
+    class ClientInfoRequest : MonoRequest
+    {
+        private const string path = "/personal/client-info";
+
+        public override string Url()
+        {
+            var sb = new StringBuilder(path);
+
+            return sb.ToString();
+        }
+    }
+}
