215bc1eded49e945877636d2b484efd686afc14e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-19 17:32:01 +0300

    Create WebhookStatus.cs

---
+++ MonoBankApi/Models/Responses/WebhookStatus.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace MonoBankApi.Models.Responses
+{
+    public class WebhookStatus
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+}
