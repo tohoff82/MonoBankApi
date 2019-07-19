a343be781ed6200c70d0e6ed3e7c3276d61a0493
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-19 15:29:09 +0300

    Update CurrencyInfoRequest.cs

--- MonoBankApi/Models/Requests/CurrencyInfoRequest.cs
+++ MonoBankApi/Models/Requests/CurrencyInfoRequest.cs
@@ -1,16 +1,10 @@
-﻿using System.Text;
+﻿using System;
 
 namespace MonoBankApi.Models.Requests
 {
     public class CurrencyInfoRequest : MonoRequest
     {
-        private const string path = "/bank/currency";
-
-        protected override string GetUrl()
-        {
-            var sb = new StringBuilder(path);
-
-            return sb.ToString();
-        }
+        protected override Uri GetUri() =>
+            new Uri("/bank/currency", UriKind.Relative);
     }
 }
