b272502753153bd05c8c2025889ce9dca8664b58
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-19 17:39:15 +0300

    Create WebhookRequest.cs

---
+++ MonoBankApi/Models/Requests/WebhookRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MonoBankApi.Models.RequestBodyes;
+
+namespace MonoBankApi.Models.Requests
+{
+    public class WebhookRequest : MonoRequest
+    {
+        public WebhookRequest(string webhookUrl) =>
+            bodyObj = new WebhookBody
+            {
+                WebHookUrl = webhookUrl
+            };
+
+        protected override Uri GetUri() =>
+            new Uri("/personal/webhook", UriKind.Relative);
+    }
+}
