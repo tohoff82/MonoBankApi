5581a44e767fcb36f36872ea219a6309be0939e8
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-23 17:37:20 +0300

    Create Currency.cs

---
+++ MonoBankApi/Models/ISO/Currency.cs
@@ -0,0 +1,10 @@
+﻿namespace MonoBankApi.Models.ISO
+{
+    class Currency
+    {
+        public string Code { get; set; }
+        public string Num { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
