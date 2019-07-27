c195308f79a7e54ad217f6cd6d749e7ca44d34b6
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-27 05:06:11 +0300

    Update MonoBank.cs

--- MonoBankApi/MonoBank.cs
+++ MonoBankApi/MonoBank.cs
@@ -1,21 +1,15 @@
 ﻿using MonoBankApi.Implements;
 using MonoBankApi.Services;
-using MonoBankApi.Serviсes;
 
 namespace MonoBankApi
 {
     public class MonoBank
     {
-        private readonly IIsoLoader isoLoader;
-
         public readonly IMonoPublic PublicService;
         public readonly IMonoPersonal PersonalService;
 
         public MonoBank(string token)
         {
-            isoLoader = new IsoLoader();
-            isoLoader.LoadData();
-
             PublicService = new MonoPublic();
             PersonalService = new MonoPersonal(token);
         }        
