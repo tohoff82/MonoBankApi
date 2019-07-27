6c6391e5e7f3b853e51e06f33139a5d7d2ae9dce
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-07-27 05:05:11 +0300

    Update Iso4217Codes.cs

--- MonoBankApi/Helper/Iso4217Codes.cs
+++ MonoBankApi/Helper/Iso4217Codes.cs
@@ -1,8 +1,23 @@
-﻿
+﻿using ISO.Library.DTO;
+using ISO.Library.Services;
+using ISO.Library.Services.Implements;
+
 namespace MonoBankApi.Helper
 {
     public static class Iso4217Codes
     {
+        static readonly ICurrencyService currencyService;
 
+        static Iso4217Codes()
+            => currencyService = new CurrencyService();
+
+        private static Currency Currency(string code)
+            => currencyService.ReturnCurrencyAsync(code).Result;
+
+        public static string GetSymbolByCode(int code)
+            => Currency(code.ToString()).Symbol;
+
+        public static string GetNameByCode(int code)
+            => Currency(code.ToString()).Name;
     }
 }
